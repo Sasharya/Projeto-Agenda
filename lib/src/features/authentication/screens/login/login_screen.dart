911496c27a2e5d1c38d5bdd87241585d1e26a1ad
Starieel <162596924+Starieel@users.noreply.github.com>
2024-03-26 14:02:58 -0300

    Initial commit

---
+++ lib/src/features/authentication/screens/login/login_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:testeagenda/src/common_widgets/form/form_header_widget.dart';
+import 'package:testeagenda/src/common_widgets/form/social_footer.dart';
+import 'package:testeagenda/src/constants/image_strings.dart';
+import 'package:testeagenda/src/constants/text_strings.dart';
+import 'package:testeagenda/src/features/authentication/screens/signup/signup_screen.dart';
+import '../../../../common_widgets/form/form_divider_widget.dart';
+import '../../../../constants/sizes.dart';
+import 'widgets/login_form_widget.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(tDefaultSpace),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const FormHeaderWidget(
+                    image: tWelcomeScreenImage,
+                    title: tLoginTitle,
+                    subTitle: tLoginSubTitle),
+                const LoginFormWidget(),
+                const TFormDividerWidget(),
+                SocialFooter(
+                    text1: tDontHaveAnAccount,
+                    text2: tSignup,
+                    onPressed: () => Get.off(() => const SignupScreen())),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
